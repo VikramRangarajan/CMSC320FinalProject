b4ba5ae20f1c1ea76ade85aec1656e824b230350
VKarGit <ved.karamsetty@gmail.com>
2024-03-26 11:10:41 -0400

    Update create_parquets.py

--- create_parquets.py
+++ create_parquets.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import os
 
 """
 This script converts the csvs into parquets for smaller size
@@ -8,4 +9,9 @@ You need to install fastparquet (run "pip install fastparquet").
 """
 
 for fn in ["job_skills", "job_summary", "linkedin_job_postings"]:
-    pd.read_csv(f"{fn}.csv").to_parquet(f"{fn}.parquet")
+    if not os.path.isfile(f"{fn}.parquet"):
+        print(f"Reading {fn}.csv to {fn}.parquet...")
+        pd.read_csv(f"{fn}.csv").to_parquet(f"{fn}.parquet")
+        print(f"{fn}.parquet created!")
+    else:
+        print(f"{fn}.parquet already exists.")
